4c371bc97076e0aa7af5c955d34edadb13169057
Joseph Blais <josephpierreblais@gmail.com>
2021-03-01 17:41:02 -0500

    Practice with SQL joins

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,5 @@
+SELECT students.name, cohorts.name as cohort_name, students.start_date as student_start_date, cohorts.start_date as cohort_start_date
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
