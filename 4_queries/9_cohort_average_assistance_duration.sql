79810574ca460d7cf400c5f1c31472b028762e27
Joseph Blais <josephpierreblais@gmail.com>
2021-03-02 16:55:03 -0500

    Add day 2 queries

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,9 @@
+SELECT avg(total_duration) as average_total_duration
+FROM (
+  SELECT cohorts.name AS name, SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assistance_requests ON students.id = student_id
+GROUP BY cohorts.name
+ORDER BY total_duration
+) as total_durations
